2252b80afa00f0f6722673eb28c51215d99515c5
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-06-25 18:14:28 +0700

    Adding ListView BUilder Chat.

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_chat/models/message_model.dart';
 import 'package:flutter_ui_chat/models/user_model.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -30,7 +31,20 @@ class _ChatPageState extends State<ChatPage> {
             top: Radius.circular(30),
           ),
           child: Column(
-            children: [],
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  padding: EdgeInsets.only(top: 15, bottom: size.height * 0.1),
+                  reverse: true,
+                  itemCount: messages.length,
+                  itemBuilder: (context, index) {
+                    final Message message = messages[index];
+                    bool isMe = message.sender.id == currentUser.id;
+                    return _buildMessage(message, isMe, size);
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
@@ -76,6 +90,64 @@ class _ChatPageState extends State<ChatPage> {
           borderRadius: BorderRadius.circular(10.0),
         ),
       ),
+    );
+  }
+
+  _buildMessage(Message message, bool isMe, Size size) {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            margin: EdgeInsets.only(
+              top: 8,
+              bottom: 8,
+              left: isMe ? size.width * 0.2 : 8,
+              right: isMe ? 8 : 0,
+            ),
+            padding: EdgeInsets.symmetric(
+              horizontal: 24,
+              vertical: 14,
+            ),
+            decoration: BoxDecoration(
+              color: isMe
+                  ? Colors.amber.withOpacity(0.2)
+                  : Colors.red.withOpacity(0.18),
+              borderRadius: isMe
+                  ? BorderRadius.only(
+                      topRight: Radius.circular(30),
+                      topLeft: Radius.circular(18),
+                      bottomLeft: Radius.circular(18),
+                    )
+                  : BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(18),
+                      bottomRight: Radius.circular(18),
+                    ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  message.time,
+                  style: GoogleFonts.poppins(
+                    color: Colors.black.withOpacity(0.4),
+                    fontSize: 11,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(height: 6),
+                Text(
+                  message.text,
+                  style: GoogleFonts.poppins(),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          width: isMe ? 0 : size.width * 0.2,
+        )
+      ],
     );
   }
 }
